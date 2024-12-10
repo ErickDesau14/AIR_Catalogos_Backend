af5457e7cf8107d0c2e9fbb748ac3de0aebb1376
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-12-09 21:23:08 -0600

    Añadiendo Tecnología Service Impl (Lógica de añadir)

--- src/main/java/com/aidr/backend/Services/Implements/TecnologiaServiceImpl.java
+++ src/main/java/com/aidr/backend/Services/Implements/TecnologiaServiceImpl.java
@@ -7,6 +7,7 @@ import com.aidr.backend.Services.Interfaces.ITecnologiaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,10 +20,78 @@ public class TecnologiaServiceImpl implements ITecnologiaService {
     @Override
     public List<TecnologiaDTO> findTecnologias() {
         return tecnologiaRepository.findAll().stream()
-                .map(tecnologia -> new TecnologiaDTO(
-                        tecnologia.getIdTecnologia(),
-                        tecnologia.getNombre(),
-                        tecnologia.isEstatus()
-                )).collect(Collectors.toList());
+                .map(tecnologia -> TecnologiaDTO.builder()
+                        .idTecnologia(tecnologia.getIdTecnologia())
+                        .nombre(tecnologia.getNombre())
+                        .estatus(tecnologia.getEstatus())
+                        .fechaCreacion(tecnologia.getFechaCreacion())
+                        .fechaModificacion(tecnologia.getFechaModificacion())
+                        .fechaBaja(tecnologia.getFechaBaja())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TecnologiaDTO findTecnologiaById(Long id) {
+        TecnologiaEntity tecnologia = tecnologiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tecnología no encontrada"));
+
+        return TecnologiaDTO.builder()
+                .idTecnologia(tecnologia.getIdTecnologia())
+                .nombre(tecnologia.getNombre())
+                .estatus(tecnologia.getEstatus())
+                .fechaCreacion(tecnologia.getFechaCreacion())
+                .fechaModificacion(tecnologia.getFechaModificacion())
+                .fechaBaja(tecnologia.getFechaBaja())
+                .build();
+    }
+
+    @Override
+    public TecnologiaDTO saveTecnologia(TecnologiaDTO tecnologiaDTO) {
+        TecnologiaEntity tecnologia = new TecnologiaEntity();
+        tecnologia.setNombre(tecnologiaDTO.getNombre());
+        tecnologia.setEstatus(1);
+        tecnologia.setFechaCreacion(new Date());
+
+        tecnologia = tecnologiaRepository.save(tecnologia);
+
+        return TecnologiaDTO.builder()
+                .idTecnologia(tecnologia.getIdTecnologia())
+                .nombre(tecnologia.getNombre())
+                .estatus(1)
+                .fechaCreacion(tecnologia.getFechaCreacion())
+                .build();
+    }
+
+    @Override
+    public TecnologiaDTO updateTecnologia(Long id, TecnologiaDTO tecnologiaDTO) {
+        TecnologiaEntity tecnologia = tecnologiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tecnología no encontrada"));
+
+        tecnologia.setNombre(tecnologiaDTO.getNombre());
+        tecnologia.setEstatus(tecnologiaDTO.getEstatus());
+        tecnologia.setFechaModificacion(new Date());
+
+        tecnologia = tecnologiaRepository.save(tecnologia);
+
+        return TecnologiaDTO.builder()
+                .idTecnologia(tecnologia.getIdTecnologia())
+                .nombre(tecnologia.getNombre())
+                .estatus(tecnologia.getEstatus())
+                .fechaCreacion(tecnologia.getFechaCreacion())
+                .fechaModificacion(tecnologia.getFechaModificacion())
+                .fechaBaja(tecnologia.getFechaBaja())
+                .build();
+    }
+
+    @Override
+    public void deleteTecnologia(Long id) {
+        TecnologiaEntity tecnologia = tecnologiaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tecnología no encontrada"));
+
+        tecnologia.setEstatus(0);
+        tecnologia.setFechaBaja(new Date());
+
+        tecnologiaRepository.save(tecnologia);
     }
 }
