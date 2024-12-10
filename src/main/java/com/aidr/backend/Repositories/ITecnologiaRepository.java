02fd41aaedf74b555a9f750b619e3340a7c9d2f2
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-12-10 12:14:16 -0600

    Interface de Tecnologia Repository

--- src/main/java/com/aidr/backend/Repositories/ITecnologiaRepository.java
+++ src/main/java/com/aidr/backend/Repositories/ITecnologiaRepository.java
@@ -2,8 +2,12 @@ package com.aidr.backend.Repositories;
 
 import com.aidr.backend.Models.TecnologiaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ITecnologiaRepository extends JpaRepository<TecnologiaEntity, Long> {
+public interface ITecnologiaRepository extends JpaRepository<TecnologiaEntity, Integer> {
+    @Query(value = "SELECT COUNT(t) > 0 FROM TecnologiaEntity t WHERE REPLACE(LOWER(t.nombre), ' ', '') = REPLACE(LOWER(:nombre), ' ', '')")
+    boolean existsByNombreIgnoreCase(@Param("nombre") String nombre);
 }
