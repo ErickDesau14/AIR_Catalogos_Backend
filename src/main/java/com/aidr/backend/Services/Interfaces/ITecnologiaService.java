baab6a587870200ac9f56a74aaa0a6ef51fd8301
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-12-10 12:15:25 -0600

    Iterface Tecnoligia Service

--- src/main/java/com/aidr/backend/Services/Interfaces/ITecnologiaService.java
+++ src/main/java/com/aidr/backend/Services/Interfaces/ITecnologiaService.java
@@ -1,12 +1,14 @@
 package com.aidr.backend.Services.Interfaces;
 
 import com.aidr.backend.DTOs.TecnologiaDTO;
-import com.aidr.backend.Models.TecnologiaEntity;
-
 import java.util.List;
 
 public interface ITecnologiaService {
 
-    public List<TecnologiaDTO> findTecnologias();
+    List<TecnologiaDTO> findTecnologias();
+    TecnologiaDTO findTecnologiaById(int id);
+    TecnologiaDTO saveTecnologia(TecnologiaDTO tecnologiaDTO);
+    TecnologiaDTO updateTecnologia(int id, TecnologiaDTO tecnologiaDTO);
+    TecnologiaDTO updateEstatusTecnologia(int id, int estatus);
 
 }
