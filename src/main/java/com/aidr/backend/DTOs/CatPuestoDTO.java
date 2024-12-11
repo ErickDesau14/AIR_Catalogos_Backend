2ac974742c04b47d08b2cce9b1c2f49dd7f7de50
Erick de la Cruz <erick@kohmi.net>
2024-12-11 02:17:02 -0600

    DTO del catálogo de puestos

--- src/main/java/com/aidr/backend/DTOs/CatPuestoDTO.java
+++ src/main/java/com/aidr/backend/DTOs/CatPuestoDTO.java
@@ -1,4 +1,21 @@
 package com.aidr.backend.DTOs;
 
-public class Cat_PuestoDTO {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CatPuestoDTO {
+    private int idPuesto;
+    private String nombre;
+    private int estatus;
+    private Date fechaCreacion;
+    private Date fechaModificacion;
+    private Date fechaBaja;
 }
