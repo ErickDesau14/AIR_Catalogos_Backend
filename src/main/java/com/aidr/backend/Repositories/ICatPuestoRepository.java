c91e874fa726c58527d8b0fd75851acf922412c0
Erick de la Cruz <erick@kohmi.net>
2024-12-11 02:14:44 -0600

    Interfaz de repositorio del catálogo de puestos

--- src/main/java/com/aidr/backend/Repositories/ICatPuestoRepository.java
+++ src/main/java/com/aidr/backend/Repositories/ICatPuestoRepository.java
@@ -1,4 +1,13 @@
 package com.aidr.backend.Repositories;
 
-public interface ICat_PuestoRepository {
+import com.aidr.backend.Models.CatPuestoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICatPuestoRepository extends JpaRepository<CatPuestoEntity, Integer> {
+    @Query(value = "SELECT COUNT(p) > 0 FROM CatPuestoEntity p WHERE REPLACE(LOWER(p.nombre), ' ', '') = REPLACE(LOWER(:nombre), ' ', '')")
+    boolean existsByNombreIgnoreCase(@Param("nombre") String nombre);
 }
