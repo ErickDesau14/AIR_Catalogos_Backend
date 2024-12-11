a7dcbf13defa848eea54412a891a2231ded0c822
Erick de la Cruz <erick@kohmi.net>
2024-12-11 02:13:09 -0600

    Interfaz de servicio de puestos

--- src/main/java/com/aidr/backend/Services/Interfaces/ICatPuestoService.java
+++ src/main/java/com/aidr/backend/Services/Interfaces/ICatPuestoService.java
@@ -1,4 +1,14 @@
 package com.aidr.backend.Services.Interfaces;
 
+import com.aidr.backend.DTOs.CatPuestoDTO;
+import java.util.List;
+
 public interface ICatPuestoService {
+
+    List<CatPuestoDTO> findPuestos();
+    CatPuestoDTO findPuestoById(int id);
+    CatPuestoDTO savePuesto(CatPuestoDTO puestoDTO);
+    CatPuestoDTO updatePuesto(int id, CatPuestoDTO puestoDTO);
+    CatPuestoDTO updateEstatusPuesto(int id, int estatus);
+
 }
