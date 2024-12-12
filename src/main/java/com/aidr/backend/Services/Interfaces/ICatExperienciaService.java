cec275240fd4bbd2c0a06416082d541982525b61
Erick de la Cruz <erick@kohmi.net>
2024-12-12 13:21:45 -0600

    Desarrollo de la interfaz de experiencia service

--- src/main/java/com/aidr/backend/Services/Interfaces/ICatExperienciaService.java
+++ src/main/java/com/aidr/backend/Services/Interfaces/ICatExperienciaService.java
@@ -1,4 +1,15 @@
 package com.aidr.backend.Services.Interfaces;
 
+import com.aidr.backend.DTOs.CatExperienciaDTO;
+
+import java.util.List;
+
 public interface ICatExperienciaService {
+
+    List<CatExperienciaDTO> findExperiencias();
+    CatExperienciaDTO findExperienciaById(int id);
+    CatExperienciaDTO saveExperiencia(CatExperienciaDTO catExperienciaDTO);
+    CatExperienciaDTO updateExperiencia(int id, CatExperienciaDTO catExperienciaDTO);
+    CatExperienciaDTO updateEstatusExperiencia(int id, int estatus);
+
 }
