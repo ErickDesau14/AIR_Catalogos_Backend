e66dcfa31f85e82aadee376a0c8cf45050b043a2
Erick de la Cruz <erick@kohmi.net>
2024-12-12 13:21:16 -0600

    Desarrollo de la interfaz repository de experiencia

--- src/main/java/com/aidr/backend/Repositories/ICatExperienciaRepository.java
+++ src/main/java/com/aidr/backend/Repositories/ICatExperienciaRepository.java
@@ -1,4 +1,13 @@
 package com.aidr.backend.Repositories;
 
-public interface ICatExperienciaRepository {
+import com.aidr.backend.Models.CatExperienciaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICatExperienciaRepository extends JpaRepository<CatExperienciaEntity, Integer> {
+    @Query("SELECT COUNT(e) > 0 FROM CatExperienciaEntity e WHERE e.anho = :anhoDeExperiencia")
+    boolean existsByAnhoDeExperiencia(@Param("anhoDeExperiencia") int anhoDeExperiencia);
 }
