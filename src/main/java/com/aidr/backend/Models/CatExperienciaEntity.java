3ab63277fdf05b5880b90058d1b8d46c77b6107f
Erick de la Cruz <erick@kohmi.net>
2024-12-12 13:20:05 -0600

    Creación de clase experiencia entitiy

--- src/main/java/com/aidr/backend/Models/CatExperienciaEntity.java
+++ src/main/java/com/aidr/backend/Models/CatExperienciaEntity.java
@@ -1,4 +1,37 @@
 package com.aidr.backend.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "cat_experiencia")
 public class CatExperienciaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idExperiencia;
+
+    private int anho;
+
+    private int estatus;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fechaCreacion;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaModificacion;
+
+    @Temporal(TemporalType.DATE)
+    private Date fechaBaja;
+
 }
