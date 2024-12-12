20afa56fb7f0c83aa2f222d3796d9bbe9b16caa2
Erick de la Cruz <erick@kohmi.net>
2024-12-12 13:19:36 -0600

    Desarrollo de la clase DTO para experiencia

--- src/main/java/com/aidr/backend/DTOs/CatExperienciaDTO.java
+++ src/main/java/com/aidr/backend/DTOs/CatExperienciaDTO.java
@@ -1,4 +1,21 @@
 package com.aidr.backend.DTOs;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CatExperienciaDTO {
+    private int idExperiencia;
+    private int anho;
+    private int estatus;
+    private Date fechaCreacion;
+    private Date fechaModificacion;
+    private Date fechaBaja;
 }
