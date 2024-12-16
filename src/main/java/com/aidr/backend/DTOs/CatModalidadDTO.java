5bf7a24213c3e49c1c7d742af46e22704b6f022e
Erick de la Cruz <erick@kohmi.net>
2024-12-16 12:41:28 -0600

    Desarrollo DTO de modalidad

---
+++ src/main/java/com/aidr/backend/DTOs/CatModalidadDTO.java
@@ -0,0 +1,23 @@
+package com.aidr.backend.DTOs;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CatModalidadDTO {
+
+    private int idModalidad;
+    private String nombre;
+    private int estatus;
+    private Date fechaCreacion;
+    private Date fechaModificacion;
+    private Date fechaBaja;
+
+}
