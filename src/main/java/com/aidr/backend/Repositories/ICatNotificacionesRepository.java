5b071cc65e46b47526a338be4690555704f35128
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-12-16 19:50:17 -0600

    Añadiendo consulta para validar si existe una misma notificación

--- src/main/java/com/aidr/backend/Repositories/ICatNotificacionesRepository.java
+++ src/main/java/com/aidr/backend/Repositories/ICatNotificacionesRepository.java
@@ -2,8 +2,12 @@ package com.aidr.backend.Repositories;
 
 import com.aidr.backend.Models.CatNotificacionesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICatNotificacionesRepository extends JpaRepository<CatNotificacionesEntity, Long> {
+public interface ICatNotificacionesRepository extends JpaRepository<CatNotificacionesEntity, Integer> {
+    @Query(value = "SELECT COUNT(n) > 0 FROM CatNotificacionesEntity n WHERE REPLACE(LOWER(n.texto), ' ', '') = REPLACE(LOWER(:texto), ' ', '')")
+    boolean existsByTextoIgnoreCase(@Param("texto") String texto);
 }
