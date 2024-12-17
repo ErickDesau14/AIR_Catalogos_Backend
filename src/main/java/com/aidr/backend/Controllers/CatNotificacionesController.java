159e31ad8c188face328691c46f0836d0d854a35
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-12-16 19:52:13 -0600

    Desarrollo del Controller de notificaciones y su conexión frontend

--- src/main/java/com/aidr/backend/Controllers/CatNotificacionesController.java
+++ src/main/java/com/aidr/backend/Controllers/CatNotificacionesController.java
@@ -1,39 +1,57 @@
 package com.aidr.backend.Controllers;
 
-import com.aidr.backend.Models.CatNotificacionesEntity;
+import com.aidr.backend.DTOs.CatNotificacionDTO;
 import com.aidr.backend.Services.Implements.CatNotificacionesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 @RestController
-@RequestMapping(path = "api/v1/Catnotificaciones")
-@CrossOrigin(origins = "http://localhost:8100", allowedHeaders = "*")
+@RequestMapping(path = "api/v1/n")
+@CrossOrigin(origins = "http://localhost:8100")
 public class CatNotificacionesController {
 
   @Autowired
   CatNotificacionesServiceImpl catNotificacionesServiceImpl;
 
-  @GetMapping
-  public List<CatNotificacionesEntity> getAll() {
-    return catNotificacionesServiceImpl.getAllNotificaciones();
+  @GetMapping("/notificacion")
+  public List<CatNotificacionDTO> findNotificaciones() {
+    return catNotificacionesServiceImpl.findNotificaciones();
   }
 
-  @GetMapping("/{idNotificacion}")
-  public Optional<CatNotificacionesEntity> getById(@PathVariable("idNotificacion") long idNotificacion) {
-    return catNotificacionesServiceImpl.getNotificacionById(idNotificacion);
+  @GetMapping("/notificacion/{id}")
+  public ResponseEntity<CatNotificacionDTO> findNotificacionById(@PathVariable int id) {
+    return new ResponseEntity<>(catNotificacionesServiceImpl.findNotificacionById(id), HttpStatus.OK);
   }
 
-  @PostMapping(consumes = "application/json", produces = "application/json")
-  public CatNotificacionesEntity saveUpdate(@RequestBody CatNotificacionesEntity notificacion) {
-    catNotificacionesServiceImpl.saveOrUpdate(notificacion);
-    return notificacion;
+  @PostMapping("/notificacion")
+  public ResponseEntity<?> createNotificacion(@RequestBody CatNotificacionDTO notificacionDTO) {
+    try {
+      CatNotificacionDTO newNotificacion = catNotificacionesServiceImpl.saveNotificacion(notificacionDTO);
+      return new ResponseEntity<>(newNotificacion, HttpStatus.CREATED);
+    } catch (RuntimeException e) {
+      return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
   }
 
-  @DeleteMapping("/{idNotificacion}")
-  public void delete(@PathVariable("idNotificacion") long idNotificacion) {
-    catNotificacionesServiceImpl.delete(idNotificacion);
+  @PutMapping("/notificacion/{id}/estatus/{estatus}")
+  public ResponseEntity<CatNotificacionDTO> updateEstatus(
+          @PathVariable int id,
+          @PathVariable int estatus
+  ) {
+    return new ResponseEntity<>(catNotificacionesServiceImpl.updateEstatusNotificacion(id, estatus), HttpStatus.OK);
   }
+
+  @PutMapping("/notificacion/{id}")
+  public ResponseEntity<CatNotificacionDTO> updateNotificacion(
+          @PathVariable int id,
+          @RequestBody CatNotificacionDTO notificacionDTO
+  ) {
+    return new ResponseEntity<>(catNotificacionesServiceImpl.updateNotificacion(id, notificacionDTO), HttpStatus.OK);
+  }
+
 }
