ff2dc6c43f4b1ad843f4b4707f4ebaa53bee91be
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-12-16 19:48:40 -0600

    Añadiendo nuevas columnas y edición del tipo de dato de Id

--- src/main/java/com/aidr/backend/Models/CatNotificacionesEntity.java
+++ src/main/java/com/aidr/backend/Models/CatNotificacionesEntity.java
@@ -1,16 +1,24 @@
 package com.aidr.backend.Models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+import java.util.Date;
+
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "CAT-Notificaciones")
 public class CatNotificacionesEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long idNotificacion;
+  private int idNotificacion;
 
   @ManyToOne
   @JoinColumn(name = "idTipoDeNotificacion", referencedColumnName = "idTipoNotificacion")
@@ -20,6 +28,15 @@ public class CatNotificacionesEntity {
 
   private int estatus;
 
+  @Column(nullable = false)
+  @Temporal(TemporalType.DATE)
+  private Date fechaCreacion;
+
+  @Temporal(TemporalType.DATE)
+  private Date fechaModificacion;
+
+  @Temporal(TemporalType.DATE)
+  private Date fechaBaja;
 
 }
 
